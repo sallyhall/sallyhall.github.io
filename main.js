fb13f2c6d5f36e394d140efc40d459a2a6df3446
Sally Kingston <sally.a.kingston@gmail.com>
2016-01-07 18:42:42 -0500

    formatting portfolio and resume

--- main.js
+++ main.js
@@ -1,29 +1,8 @@
-var $ = require('jquery')(window);
-global.jQuery = require("jquery");
-var _ = require('underscore');
-var bootstrap = require('bootstrap/dist/js/bootstrap');
-
-var portfolioPage = {
-  init: function () {
-    portfolioPage.styling();
-    portfolioPage.events();
-  },
-  events: function () {
-
-  },
-  styling:  function(){
-    _.each(projects, function (project) {
-      if(project.display){
-        portfolioPage.loadTemplate("#projectTmpl",project,"#projects");
-      }
-    })
-  },
-  loadTemplate: function (template,object,element) {
-    var temp = _.template($(template).html());
-    $(element).append(temp(object));
-  }
-};
-
-$(document).ready(function () {
-  portfolioPage.init();
-});
+(function() {
+  'use strict';
+  $(".workItem").on("click",".fa",function(){
+      $(this).toggleClass("fa-plus");
+      $(this).toggleClass("fa-minus");
+      $(this).siblings("ul").children().toggleClass("hidden");
+  });
+}());
